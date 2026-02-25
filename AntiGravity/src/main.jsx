e66f9a0d629406efecbaa1abb93c534c9d515e6f
Revti Shah <rushabhshah011@gmail.com>
2026-02-25 09:52:07 +0530

    fix(routing): use HashRouter for robust SPA routing on GitHub Pages

--- AntiGravity/src/main.jsx
+++ AntiGravity/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import AdminApp from './AdminApp.jsx'
 import App from './App.jsx' // Renamed from UserApp to App based on the instruction's code edit
 import { GlobalSessionProvider } from './context/GlobalSessionContext.jsx'
@@ -9,7 +9,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalSessionProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           {/* User Application Routes */}
           <Route path="/" element={<App />} />
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           {/* Admin Application Route */}
           <Route path="/admin/*" element={<AdminApp />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </GlobalSessionProvider>
   </React.StrictMode>,
 )
